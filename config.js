e64c0fc0862b17fe78f72b33ba01d3238d036e1b
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-05-28 14:28:38 -0400

    Update Site

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ const config = {
     "mainCTAButtonURL": "https://calendly.com/rick-roccasalva_co/discovery-call",
     "primaryColor": "#e3c379",
     "accentColor": "#d9c6a2",
-    "facebookPixelId": ""
+    "facebookPixelId": "1437870807350352"
   },
   "header": {
     "tag": "CASE STUDY",
@@ -43,7 +43,7 @@ const config = {
     "sidebar": {
       "title": "About {{companyName}}",
       "image": {
-        "src": "https://salesgenius.s3.ca-central-1.amazonaws.com/uploads/1761767184484-biorick.jpg"
+        "src": "https://salesgeniusportal.s3.ca-central-1.amazonaws.com/TeamUploads/upload-4F255BBD-7E78-4D67-9BFB-675A94665BAF.png"
       },
       "paragraphs": [
         "At Roccasalva & Co., we are built on integrity, insight, and innovation. We combine decades of real estate expertise with cutting-edge technology to deliver unmatched support, transparency, and results for our clients.\n\nWith over 40 years of hands-on real estate experience and extensive corporate backgrounds, our managing partners provide a balanced and strategic approach to Residential, Commercial, and Investment properties.\n\nWe prioritize clear communication and use the latest market insights to guide every client toward success - whether buying, selling, or investing.\n\nAt Roccasalva & Co., we’re more than real estate experts - we’re your trusted partner in building your future."
